dae3c213c9345d11a49c7b3ef169bf307005ddb2
MIDAS <70561738+MIDAS2020@users.noreply.github.com>
2021-03-15 11:14:18 +0800

    Delete Catapult++ directory

--- Catapult++/MidasMain.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "SmallGraphClustering.h"
-string LOGFILENAME = "MidasLogFile.txt";
-ofstream LOGFILE;
-//string CLUSTERFILENAME = "MidasClusterFile.txt";
-//ofstream CLUSTERFILE;
-
